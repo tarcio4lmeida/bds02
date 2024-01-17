662f675c6642917627aca3f2cec9c515bf80ea86
tarcio almeida <tarcioalmeidalima@gmail.com>
2024-01-17 00:26:41 -0300

    Segundo teste passando

--- src/main/java/com/devsuperior/bds02/controller/CityController.java
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -3,14 +3,14 @@ package com.devsuperior.bds02.controller;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cities")
@@ -27,5 +27,17 @@ public class CityController {
         dto = service.insert(dto);
         return ResponseEntity.created(uri).body(dto);
     }
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<CityDTO> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
--- src/main/java/com/devsuperior/bds02/service/CityService.java
+++ src/main/java/com/devsuperior/bds02/service/CityService.java
@@ -4,8 +4,13 @@ import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
@@ -21,4 +26,11 @@ public class CityService {
         return new CityDTO(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        return repository.findAll(Sort.by("name")).stream().map(CityDTO::new).collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+    }
 }
