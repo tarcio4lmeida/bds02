1675ddfeeb19be9a7133339f43abdff51ce8398a
tarcio almeida <tarcioalmeidalima@gmail.com>
2024-01-17 00:19:46 -0300

    Teste Insert City passando

---
+++ src/main/java/com/devsuperior/bds02/controller/CityController.java
@@ -0,0 +1,31 @@
+package com.devsuperior.bds02.controller;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        dto = service.insert(dto);
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+}
---
+++ src/main/java/com/devsuperior/bds02/service/CityService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.service;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+
+}
